c4627874034d3ee595f413001359157f36466048
Sen1Gao <sengao1024@outlook.com>
2025-02-14 19:15:38 +1100

    Frontend startup

---
+++ ImageDisplaySystem/Frontend/ViewModels/MianWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.ViewModels
+{
+    public class MianWindowViewModel : INotifyPropertyChanged
+    {
+        public MianWindowViewModel() {}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
